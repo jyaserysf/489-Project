96fc7e74728c0719378b21b6583dc87afec31c74
jyaserysf <[jood.yaser.yusuf@gmail.com]>
2023-04-19 03:17:47 +0300

    added css and js folders

---
+++ js/sidenav.js
@@ -0,0 +1,11 @@
+/* Set the width of the side navigation to 270px and the left margin of the page content to 270px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "270px";
+    document.getElementById("main").style.marginLeft = "270px";
+} 
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0"; 
+    document.getElementById("main").style.marginLeft = "0";
+}
